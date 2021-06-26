249df8e95b74427137b2da37ead4cea36d97f8db
TELEUZI <cosmosik@inbox.ru>
2021-06-26 16:03:28 +0300

    feat: update webpack

--- chess/.eslintrc.js
+++ chess/.eslintrc.js
@@ -5,7 +5,6 @@ module.exports = {
     es2021: true,
   },
   extends: [
-    'airbnb-base',
     'airbnb-typescript/base',
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
--- chess/webpack.config.js
+++ chess/webpack.config.js
@@ -1,32 +1,72 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
 
-module.exports = {
-  entry: './src/app.ts',
-  module: {
-    rules: [{
-      test: [/\.tsx?$/],
-      use: ['ts-loader'],
-      exclude: /node_modules/,
-    }, {
-      test: /.scss$|.css/,
-      exclude: [/node_modules/],
-      use: ['style-loader', 'css-loader', 'sass-loader']
-    }, {
-      test: /.(?:ico|gif|png|jpg|svg)$/,
-      type: 'asset/resource',
-    }],
-  },
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
-  },
-  mode: 'development',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
+const devServer = (isDev) => !isDev ? {} : {
   devServer: {
-    contentBase: path.join(__dirname, ''),
-    compress: true,
-    port: 4000,
+    open: true,
+    hot: true,
+    port: 8080,
+    contentBase: path.join(__dirname, 'public'),
   },
 };
+
+const esLintPlugin = (isDev) => isDev ? [] : [new ESLintPlugin({ extensions: ['ts', 'js'] })];
+
+module.exports = ({ development }) => ({
+  mode: development ? 'development' : 'production',
+  devtool: development ? 'inline-source-map' : false,
+  entry: {
+    main: './src/index.ts',
+  },
+  output: {
+    filename: '[name].[contenthash].js',
+    path: path.resolve(__dirname, 'dist'),
+    assetModuleFilename: 'assets/[hash][ext]',
+  },
+  module: {
+    rules: [{
+        test: /\.[tj]s$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(?:ico|gif|png|jpg|jpeg|svg)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff(2)?|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+      }
+    ],
+  },
+  plugins: [
+    ...esLintPlugin(development),
+    new MiniCssExtractPlugin({ filename: '[name].[contenthash].css' }),
+    new HtmlWebpackPlugin({ title: 'Chess' }),
+    new CopyPlugin({
+
+      patterns: [{
+        from: 'public',
+        to: path.resolve(__dirname, "./dist/assets"),
+        noErrorOnMissing: true,
+      }, ],
+    }),
+    new CleanWebpackPlugin({ cleanStaleWebpackAssets: false }),
+  ],
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  ...devServer(development)
+});
